26f29700f84e7fffc62ed4a77dcfe9a42eeb723d
no-im-bk <80921911+no-im-bk@users.noreply.github.com>
2025-01-30 16:29:12 -0700

    added guessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Number guessing game");
+
+    println!("Please guess an integer from 1 to 100");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed {}", guess);
 }
